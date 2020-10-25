53a47529158e41f5329bcdba2efbc11e3ad32519
bentaro <swd.ben@gmail.com>
2020-10-25 16:33:26 +0900

    add cosmwasm vm

---
+++ x/nftchain/genesis.go
@@ -0,0 +1,22 @@
+package nftchain
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/bentaro/nftchain/x/nftchain/keeper"
+	"github.com/bentaro/nftchain/x/nftchain/types"
+	// abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// InitGenesis initialize default parameters
+// and the keeper's address to pubkey map
+func InitGenesis(ctx sdk.Context, k keeper.Keeper /* TODO: Define what keepers the module needs */, data types.GenesisState) {
+	// TODO: Define logic for when you would like to initalize a new genesis
+}
+
+// ExportGenesis writes the current store values
+// to a genesis file, which can be imported again
+// with InitGenesis
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) (data types.GenesisState) {
+	// TODO: Define logic for exporting state
+	return types.NewGenesisState()
+}
